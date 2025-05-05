44fcafb906c05cebf4f9ca48cd13a7d86ea073f5
Gustavo Calanzani <gustavo.calanzani@autoglass.com.br>
2025-05-05 08:42:47 -0300

    exemplo uso API

---
+++ API-modeloML/exemploUso.py
@@ -0,0 +1,16 @@
+######## Exemplo de uso #########
+
+modelo = joblib.load(r'../API-modeloML/modeloFinal.pkl')
+vetorizadorTFIDF = joblib.load(r'../API-modeloML/vetorizador.pkl')
+
+texto = "texto qualquer licitação licitacao licitacao"
+texto_tfidf = vetorizadorTFIDF.transform([texto])
+feature_names = vetorizadorTFIDF.get_feature_names_out()
+texto_df = pd.DataFrame(texto_tfidf.toarray(), columns=feature_names)
+
+resultado = modelo.predict(texto_df)
+print(resultado)
+# 'contrato': 1
+# 'licitacao': 2
+# 'notaempenho': 3
+# 'sancao': 4
